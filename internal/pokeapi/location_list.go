e08cc8bb9172bfccbd9148dcc50d4e9593b6c96c
Kanatello7 <jetrukanatello@gmail.com>
2025-12-03 00:00:33 +0500

    added cache and updated api code to use cache

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -6,33 +6,39 @@ import (
 	"net/http"
 )
 
+func (c *Client) getAndCache(url string, target *RespShallowLocations) error {
+	if cached, ok := c.cache.Get(url); ok {
+		return json.Unmarshal(cached, target)
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(dat, target); err != nil {
+		return err
+	}
+	c.cache.Add(url, dat)
+	return nil
+}
+
 func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 	url := baseURL + "/location-area"
 	if pageURL != nil {
 		url = *pageURL
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
-	defer resp.Body.Close()
-
-	dat, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
-
-	locationsResp := RespShallowLocations{}
-	err = json.Unmarshal(dat, &locationsResp)
-	if err != nil {
-		return RespShallowLocations{}, err
-	}
-
-	return locationsResp, nil
+	var resp RespShallowLocations
+	err := c.getAndCache(url, &resp)
+	return resp, err
 }
